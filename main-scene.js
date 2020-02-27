6418a76072d4c0c79d77ad33a791c24a64a7d0c6
mariechu3 <mariechu3@gmail.com>
2020-02-27 15:57:33 -0800

    put bounds so object cannot go through the mirror, need to find some way to put a wall or something so you can't see the avatar in between mirrors. Need to work on physics behind convex and concave mirrors

--- main-scene.js
+++ main-scene.js
@@ -38,10 +38,12 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
                 {
                     avatar: context.get_instance(Phong_Shader).material(Color.of(0,0,1,1), {ambient: 0.0}),
                     wall: context.get_instance(Phong_Shader).material(Color.of(1,1,1,1), {ambient: 1}),
-                    mirror: context.get_instance(Phong_Shader).material(Color.of(.95,1,.95,1), {ambient:0.0, diffusivity: 0})
+                    mirror: context.get_instance(Phong_Shader).material(Color.of(.95,1,.95,1), {ambient:0.05, diffusivity: 0})
                 };
 
-            this.lights = [new Light(Vec.of(0, 10, 0, 1), Color.of(1, 1, 1, 1), 1000),
+            this.lights = [new Light(Vec.of(0, 10, 0, 1), Color.of(1, 1, 1, 1), 100000),
+                           new Light(Vec.of(-20, 10, 0, 1), Color.of(1, 1, 1, 1), 100000),
+                           new Light(Vec.of(20, 10, 0, 1), Color.of(1, 1, 1, 1), 100000),
                            new Light(Vec.of(0, 8, 30, 1), Color.of(1, 1, 1, 1), 1000),
                            new Light(Vec.of(0, 8, -30, 1), Color.of(1, 1, 1, 1), 1000)];
 
@@ -49,6 +51,8 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
             this.move_l_pressed = false;
             this.move_u_pressed = false;
             this.move_d_pressed = false;
+            this.move_f_pressed = false;
+            this.move_b_pressed = false;
         }
         set_pos(dir)
         {
@@ -58,22 +62,39 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
                 this.avatar_pos=this.avatar_pos.times(Mat4.translation([0,.5,0]));
             else if(dir==3)
                 this.avatar_pos=this.avatar_pos.times(Mat4.translation([0,-.5,0]));
+            else if(dir==4) {
+                if(this.avatar_pos[2][3]>1.0)
+                    this.avatar_pos = this.avatar_pos.times(Mat4.translation([0, 0, -.5]));
+            }
+            else if(dir==5)
+                this.avatar_pos=this.avatar_pos.times(Mat4.translation([0,0,.5]));
             else
                 this.avatar_pos=this.avatar_pos.times(Mat4.translation([.5,0,0]));
+            console.log(this.avatar_pos[0]);
+            console.log(this.avatar_pos[1]);
+            console.log(this.avatar_pos[2]);
+            console.log(this.avatar_pos[3]);
+            console.log("n");
             this.move_l_pressed = false;
             this.move_r_pressed = false;
             this.move_u_pressed = false;
             this.move_d_pressed = false;
+            this.move_f_pressed = false;
+            this.move_b_pressed = false;
+
         }
 
         make_control_panel() {
             // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
 
-            this.key_triggered_button("Move Avatar Right", ["5"], () => {this.move_r_pressed = true});
-            this.key_triggered_button("Move Avatar Left", ["6"], () => {this.move_l_pressed = true});
+            this.key_triggered_button("Move Avatar Right", ["4"], () => {this.move_r_pressed = true});
+            this.key_triggered_button("Move Avatar Left", ["5"], () => {this.move_l_pressed = true});
             this.new_line();
-            this.key_triggered_button("Move Avatar Up", ["7"], () => {this.move_u_pressed = true});
-            this.key_triggered_button("Move Avatar Down", ["8"], () => {this.move_d_pressed = true});
+            this.key_triggered_button("Move Avatar Up", ["6"], () => {this.move_u_pressed = true});
+            this.key_triggered_button("Move Avatar Down", ["7"], () => {this.move_d_pressed = true});
+            this.new_line();
+            this.key_triggered_button("Move Avatar Forward", ["8"], () => {this.move_f_pressed = true});
+            this.key_triggered_button("Move Avatar Back", ["9"], () => {this.move_b_pressed = true});
             this.new_line();
             this.new_line();
             this.new_line();
@@ -83,7 +104,7 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
             this.key_triggered_button("Attach to mirror 2", ["2"], () => this.attached = () => this.mirror_2);
             this.new_line();
             this.key_triggered_button("Attach to mirror 3", ["3"], () => this.attached = () => this.mirror_3);
-            this.key_triggered_button("Attach to avatar 4", ["a"], () => this.attached = () => this.avatar);
+            this.key_triggered_button("Attach to avatar 4", ["a"], () => this.attached = () => this.avatar_pos);
             this.new_line();
         }
 
@@ -92,8 +113,14 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
             const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
             let identity = Mat4.identity();
             // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 2 and 3)
-            this.shapes.mirror.draw(graphics_state,identity.times(Mat4.scale([.25,.5,.05])),this.materials.mirror);
+            this.mirror_1 = identity.times(Mat4.scale([.25,.5,.05]));
+            this.shapes.mirror.draw(graphics_state,this.mirror_1,this.materials.mirror);
+            this.mirror_2 = identity.times(Mat4.translation([-10,0,0])).times(Mat4.scale([.25,.5,.05]));
+            this.shapes.mirror.draw(graphics_state,this.mirror_2,this.materials.mirror);
+            this.mirror_3 = identity.times(Mat4.translation([10,0,0])).times(Mat4.scale([.25,.5,.05]));
+            this.shapes.mirror.draw(graphics_state,this.mirror_3,this.materials.mirror);
             //this.shapes.box.draw(graphics_state,identity.times(Mat4.scale([100,100,.05])),this.materials.wall);
+            // movement of avatar
             if(this.move_r_pressed)
                 this.set_pos(0);
             else if(this.move_l_pressed)
@@ -102,20 +129,43 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
                 this.set_pos(2);
             else if(this.move_d_pressed)
                 this.set_pos(3);
+            else if(this.move_f_pressed)
+                this.set_pos(4);
+            else if(this.move_b_pressed)
+                this.set_pos(5);
+
             this.shapes.sphere4.draw(graphics_state,this.avatar_pos,this.materials.avatar);
-            this.shapes.sphere4.draw(graphics_state,this.avatar_pos.times(Mat4.translation([0,0,-10])),this.materials.avatar);
+            //plane mirror
+            let copy = this.avatar_pos.times(Mat4.scale([.5,.5,.5]));
+            let reflected_mat = [this.avatar_pos[0],
+                               this.avatar_pos[1],
+                              [-1*this.avatar_pos[2][0],-1*this.avatar_pos[2][1],-1*this.avatar_pos[2][2],-1*this.avatar_pos[2][3]],
+                               this.avatar_pos[3]];
+            //plane mirror
+            if(this.avatar_pos[0][3]>= -4 && this.avatar_pos[0][3] <= 4)
+                this.shapes.sphere4.draw(graphics_state,reflected_mat,this.materials.avatar);
+            //convex + concave just have placeholder scalars for now, do the actual physical simulation later
+            reflected_mat = [copy[0],
+                copy[1],
+                [-1*copy[2][0],-1*copy[2][1],-1*copy[2][2],-1*copy[2][3]],
+                copy[3]];
+            if(this.avatar_pos[0][3]<-4)
+                this.shapes.sphere4.draw(graphics_state,reflected_mat,this.materials.avatar);
 
-
+            if(this.avatar_pos[0][3]> 4) {
+                this.shapes.sphere4.draw(graphics_state,reflected_mat, this.materials.avatar);
+            }
 
 
 
             //camera coordinates
-            let translate_back = Mat4.translation(Vec.of(0,0,5));
+            let translate_back = Mat4.translation(Vec.of(0,0,10));
             if(typeof this.attached !== 'undefined') {
                 switch (this.attached()) {
                     case (this.initial_camera_location):
                         graphics_state.camera_transform = Mat4.look_at(Vec.of(0, 5, 30), Vec.of(0, 0, 0), Vec.of(0, 1, 0)).map((x,i) => Vec.from(graphics_state.camera_transform[i]).mix(x,0.1));
                         break;
+                        //these don't work, probably because of the scaling
                         /*
                     case (this.mirror_1):
                         graphics_state.camera_transform = Mat4.inverse(this.mirror_1.times(translate_back)).map((x,i) => Vec.from(graphics_state.camera_transform[i]).mix(x,0.1));
@@ -127,8 +177,9 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
                         graphics_state.camera_transform = Mat4.inverse(this.mirror_3.times(translate_back)).map((x,i) => Vec.from(graphics_state.camera_transform[i]).mix(x,0.1));
                         break;
                     case (this.avatar):
-                        graphics_state.camera_transform = Mat4.inverse(this.avatar.times(translate_back)).map((x,i) => Vec.from(graphics_state.camera_transform[i]).mix(x,0.1));
-                        break;*/
+                        graphics_state.camera_transform = Mat4.inverse(this.avatar_pos.times(translate_back)).map((x,i) => Vec.from(graphics_state.camera_transform[i]).mix(x,0.1));
+                        break;
+                         */
                 }
 
             }
@@ -136,89 +187,3 @@ window.Mirror_Scene= window.classes.Mirror_Scene =
         }
     };
 
-
-// Extra credit begins here (See TODO comments below):
-
-window.Ring_Shader = window.classes.Ring_Shader =
-    class Ring_Shader extends Shader {
-        // Subclasses of Shader each store and manage a complete GPU program.
-        material() {
-            // Materials here are minimal, without any settings.
-            return {shader: this}
-        }
-
-        map_attribute_name_to_buffer_name(name) {
-            // The shader will pull single entries out of the vertex arrays, by their data fields'
-            // names.  Map those names onto the arrays we'll pull them from.  This determines
-            // which kinds of Shapes this Shader is compatible with.  Thanks to this function,
-            // Vertex buffers in the GPU can get their pointers matched up with pointers to
-            // attribute names in the GPU.  Shapes and Shaders can still be compatible even
-            // if some vertex data feilds are unused.
-            return {object_space_pos: "positions"}[name];      // Use a simple lookup table.
-        }
-
-        // Define how to synchronize our JavaScript's variables to the GPU's:
-        update_GPU(g_state, model_transform, material, gpu = this.g_addrs, gl = this.gl) {
-            const proj_camera = g_state.projection_transform.times(g_state.camera_transform);
-            // Send our matrices to the shader programs:
-            gl.uniformMatrix4fv(gpu.model_transform_loc, false, Mat.flatten_2D_to_1D(model_transform.transposed()));
-            gl.uniformMatrix4fv(gpu.projection_camera_transform_loc, false, Mat.flatten_2D_to_1D(proj_camera.transposed()));
-        }
-
-        shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        {
-            return `precision mediump float;
-              varying vec4 position;
-              varying vec4 center;
-      `;
-        }
-
-        vertex_glsl_code()           // ********* VERTEX SHADER *********
-        {
-            return `
-        attribute vec3 object_space_pos;
-        uniform mat4 model_transform;
-        uniform mat4 projection_camera_transform;
-
-        void main()
-        { 
-        position = vec4(object_space_pos,1.0);
-        center = vec4(0.0,0.0,0.0,1.0);
-        gl_Position = projection_camera_transform* model_transform * position;
-        }`;           // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
-        }
-
-        fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-        {
-            return `
-        void main()
-        { 
-        if(distance(position,center) <= 2.5)
-        {
-        if(distance(position,center) >= 1.26)
-        {
-        if(sin(30.0* distance(position,center)) > 0.0)
-            gl_FragColor = vec4(0.0,0.0,0.0,1.0);
-        else
-            gl_FragColor = vec4(.5,.3,.2,1.0);
-        }
-        }
-        }`;           // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
-        }
-    };
-
-window.Grid_Sphere = window.classes.Grid_Sphere =
-    class Grid_Sphere extends Shape           // With lattitude / longitude divisions; this means singularities are at
-    {
-        constructor(rows, columns, texture_range)             // the mesh's top and bottom.  Subdivision_Sphere is a better alternative.
-        {
-            super("positions", "normals", "texture_coords");
-            // TODO:  Complete the specification of a sphere with lattitude and longitude lines
-            //        (Extra Credit Part III)
-            const semi_circle_points = Array(rows).fill(Vec.of(0,0,1))
-                .map((p, i, a) =>
-                    Mat4.rotation(i / (a.length - 1) * Math.PI, Vec.of(0, -1, 0))
-                    .times(p.to4(1)).to3());
-            Surface_Of_Revolution.insert_transformed_copy_into(this,[rows,columns,semi_circle_points]);
-        }
-    };
