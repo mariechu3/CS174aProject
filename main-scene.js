6e9d44886ef44d50abaef1ee59439c37e1e95e92
swa770-ucla <swa770@g.ucla.edu>
2020-02-29 17:23:53 -0800

    dart power and angle control
    Finished implementing power and angle control

--- main-scene.js
+++ main-scene.js
@@ -241,9 +241,8 @@ window.Dart_Scene= window.classes.Dart_Scene =
             const shapes = {
                 torus: new Torus(15, 15),
                 torus2: new (Torus.prototype.make_flat_shaded_version())(15, 15),
-
+                bar: new Cube(),
                 cylinder: new Rounded_Capped_Cylinder(15,15,[2,2]),
-
             };
             this.submit_shapes(context, shapes);
 
@@ -254,6 +253,7 @@ window.Dart_Scene= window.classes.Dart_Scene =
                     wall: context.get_instance(Phong_Shader).material(Color.of(.8,.9,1,1), {ambient: 1}),
                     mirror: context.get_instance(Phong_Shader).material(Color.of(.95,1,.95,1), {ambient:0.1, diffusivity: 0}),
                     floor: context.get_instance (Phong_Shader).material( Color.of (0,0,0,1), {ambient: 0.3, texture: context.get_instance("assets/tile.jpg",true)}),
+                    bar: context.get_instance(Phong_Shader).material(Color.of(1,1,1,1), {ambient: 1}),
                 };
 
             this.lights = [new Light(Vec.of(0, 10, 0, 1), Color.of(1, 1, 1, 1), 100000),
@@ -264,6 +264,19 @@ window.Dart_Scene= window.classes.Dart_Scene =
 
             this.move_l_pressed = this.move_r_pressed = this.move_u_pressed =
                 this.move_d_pressed = this.move_f_pressed = this.move_b_pressed = false;
+
+
+            this.max_power = 100;
+            this.power = 0;
+            this.charging = false;
+            this.charging_start_time = 0;
+            this.curr_time = 0;
+            this.bar_transform = Mat4.identity().times(Mat4.translation([10,0,0]));
+
+            this.angle = 0; // max 90
+            this.angle_up_start = false;
+            this.angle_down_start = false;
+
         }
         set_pos(dir)
         {
@@ -291,6 +304,14 @@ window.Dart_Scene= window.classes.Dart_Scene =
         make_control_panel() {
             // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
 
+            this.key_triggered_button("Charge", ["Enter"], this.start_charging, '#'+Math.random().toString(9).slice(-6),this.shoot);
+            this.new_line();
+
+            this.key_triggered_button("Angle Up", ["k"], this.angle_up, '#'+Math.random().toString(9).slice(-6), this.angle_up_release);
+            this.key_triggered_button("Angle Down", ["l"], this.angle_down, '#'+Math.random().toString(9).slice(-6), this.angle_down_release);
+
+            this.new_line();
+
             this.key_triggered_button("Move Avatar Left", ["4"], () => {this.move_l_pressed = true});
             this.key_triggered_button("Move Avatar Right", ["5"], () => {this.move_r_pressed = true});
             this.new_line();
@@ -312,19 +333,66 @@ window.Dart_Scene= window.classes.Dart_Scene =
             this.new_line();
         }
 
+        start_charging() {
+            console.log("Start charging");
+            this.charging = true;
+            this.charging_start_time = this.curr_time;
+        }
+
+        shoot() {
+            console.log("Shoot!");
+            console.log(this.power);
+            this.charging = false;
+        }
+        angle_up() {
+            if (!this.angle_down_start) {
+                this.angle_up_start = true;
+            }
+        }
+        angle_up_release() {
+            this.angle_up_start = false;
+        }
+        angle_down() {
+            if (!this.angle_up_start) {
+                this.angle_down_start = true;
+            }
+        }
+        angle_down_release() {
+            this.angle_down_start = false;
+        }
+        set_angle() {
+            if (this.angle_up_start && this.angle < 100) {
+                this.angle = this.angle + 1;
+            } else if (this.angle_down_start && this.angle > 0) {
+                this.angle = this.angle - 1;
+            }
+        }
+        set_bar_transform() {
+            this.bar_transform = Mat4.identity()
+                .times(Mat4.scale([1,10*(this.power/this.max_power),1]))
+                .times(Mat4.translation([10,0,0]))
+        }
+
         display(graphics_state) {
             graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
             const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
 
+            // charging
+            if (this.charging) {
+                this.power =  this.max_power * 0.5 * (Math.sin((t-this.charging_start_time) - 0.5 * Math.PI) + 1);
+            } else {
+                this.curr_time = t;
+            }
+
+            this.set_angle();
+            this.set_bar_transform();
+
+            this.shapes.bar.draw(graphics_state, this.bar_transform, this.materials.bar);
             // Create Cylinder
-
-
+            
             let identity = Mat4.identity();
-
             //draw avatar
             this.shapes.cylinder.draw(graphics_state,this.avatar_pos,this.materials.avatar);
-
-
         }
     };
 
